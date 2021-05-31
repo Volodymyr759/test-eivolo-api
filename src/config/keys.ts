b5594aceaf1d41b201fed326baef955b925dc461
Volodia <volodymyr.m@eivolo.com>
2021-05-31 16:39:05 +0300

    init

---
+++ src/config/keys.ts
@@ -0,0 +1,4 @@
+export default {
+    mongoUri: 'mongodb+srv://volodymyr:Adscasc21351-@cluster0.dra32.mongodb.net/eivolodb?retryWrites=true&w=majority',
+    localPort: 3000
+};
---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import config from './config/keys';
+
+const PORT = process.env.port || config.localPort;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+    await app.listen(PORT);
+}
+bootstrap();
