18473fc0584546d39ca2d234924022c6b4c10d31
Volodia <volodymyr.m@eivolo.com>
2021-05-31 18:19:12 +0300

    changes

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ItemsController } from './items/items.controller';
-import { ItemsService } from './items/items.service';
-import { ItemsModule } from './items/items.module';
 import { MessagesController } from './messages/messages.controller';
 import { MessagesModule } from './messages/messages.module';
 import { MessagesService } from './messages/messages.service';
@@ -11,8 +8,8 @@ import { MongooseModule } from '@nestjs/mongoose';
 import config from './config/keys';
 
 @Module({
-    imports: [ItemsModule, MessagesModule, MongooseModule.forRoot(config.mongoUri)],
-    controllers: [AppController, ItemsController, MessagesController],
-    providers: [AppService, ItemsService, MessagesService],
+    imports: [MessagesModule, MongooseModule.forRoot(config.mongoUri)],
+    controllers: [AppController, MessagesController],
+    providers: [AppService, MessagesService],
 })
-export class AppModule {}
+export class AppModule { }
