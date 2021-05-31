4ce4a4d8260bdb9b40ccc88cf365af961a9b2c26
Volodia <volodymyr.m@eivolo.com>
2021-05-31 17:11:43 +0300

    asd

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import config from './config/keys';
 
-const PORT = process.env.port || config.localPort;
+const PORT = process.env.PORT || config.localPort;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
