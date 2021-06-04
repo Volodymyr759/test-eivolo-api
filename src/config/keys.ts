01ad904cb7b6e9fe3026b261195868f1e49f28f1
Volodia <volodymyr.m@eivolo.com>
2021-06-04 12:36:31 +0300

    implemented CORS

--- src/config/keys.ts
+++ src/config/keys.ts
@@ -1,4 +1,4 @@
 export default {
     mongoUri: 'mongodb+srv://volodymyr:Adscasc21351-@cluster0.dra32.mongodb.net/eivolodb?retryWrites=true&w=majority',
-    localPort: 3000
+    localPort: 3001
 };
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import config from './config/keys';
+import cors from 'cors';
 
 const PORT = process.env.PORT || config.localPort;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-    await app.listen(PORT);
+    const app = await NestFactory.create(AppModule);
+
+    const allowedOrigins = ['http://localhost:3000'];
+    const options: cors.CorsOptions = { origin: allowedOrigins };
+    app.use(cors(options));
+
+    await app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
     // 
 }
 bootstrap();
