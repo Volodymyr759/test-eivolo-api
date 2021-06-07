615b87c214178d351ae5ee0d4dc86609eb46efa5
Volodia <volodymyr.m@eivolo.com>
2021-06-07 13:50:30 +0300

    restore dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MessagesController } from './messages/messages.controller';
 import { MessagesModule } from './messages/messages.module';
-import { MessagesService } from './messages/messages.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import config from './config/keys';
 
 @Module({
     imports: [MessagesModule, MongooseModule.forRoot(config.mongoUri)],
-    controllers: [AppController, MessagesController],
-    providers: [AppService, MessagesService],
+    controllers: [AppController],
+    providers: [AppService],
 })
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import config from './config/keys';
-import cors from 'cors';
 
 const PORT = process.env.PORT || config.localPort;
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-
-    const allowedOrigins = ['http://localhost:3000'];
-    const options: cors.CorsOptions = { origin: allowedOrigins };
-    app.use(cors(options));
 
     await app.listen(PORT, () => console.log(`Server started on port: ${PORT}`));
     // 
