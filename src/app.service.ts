96d2bd14714acaef1fe71faabea8dad293e5c1f5
Volodia <volodymyr.m@eivolo.com>
2021-06-08 15:24:02 +0300

    minor changes

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getTestMessage(): string {
-      return 'Test Message.';
+      return 'Test message.';
   }
 }
