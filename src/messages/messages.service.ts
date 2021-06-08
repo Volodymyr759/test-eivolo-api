af8ed47249966fdd9dc5b40bda1ade4a29feae59
Volodia <volodymyr.m@eivolo.com>
2021-06-08 16:45:34 +0300

    implemented mapping

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -4,7 +4,6 @@ import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { HttpException, HttpStatus } from '@nestjs/common'
 import { ServiceResult } from '../infrastructure/ServiceResult';
-//import { resolveAny } from 'dns';
 
 @Injectable()
 export class MessagesService {
@@ -17,7 +16,7 @@ export class MessagesService {
     async findAll(): Promise<ServiceResult<Message>> {
         await this.messageModel.find()
             .then(
-                result => { this.setResult(HttpStatus.OK, 'Ok', true, result); }
+                result => { this.setResult(HttpStatus.OK, 'Ok', true, this.mapMessageSchemaToDto(result)) }
             )
             .catch(
                 () => {
@@ -32,7 +31,7 @@ export class MessagesService {
     async findById(id: string): Promise<ServiceResult<Message>> {
         await this.messageModel.find({ _id: id })
             .then(
-                result => { this.setResult(HttpStatus.OK, 'Ok', true, result); }
+                result => { this.setResult(HttpStatus.OK, 'Ok', true, this.mapMessageSchemaToDto(result)); }
             )
             .catch(
                 () => {
@@ -50,7 +49,7 @@ export class MessagesService {
             .then(
                 result => {
                     let data = [{ result }]
-                    this.setResult(HttpStatus.CREATED, 'Created', true, data);
+                    this.setResult(HttpStatus.CREATED, 'Created', true, this.mapMessageSchemaToDto(data));
                 }
             )
             .catch(
@@ -76,7 +75,7 @@ export class MessagesService {
             .then(
                 result => {
                     let data = [{ result }]
-                    this.setResult(HttpStatus.OK, 'Ok', true, data);
+                    this.setResult(HttpStatus.OK, 'Ok', true, this.mapMessageSchemaToDto(data));
                 }
             );
 
@@ -96,7 +95,7 @@ export class MessagesService {
             .then(
                 result => {
                     let data = [{ result }]
-                    this.setResult(HttpStatus.OK, 'Ok', true, data);
+                    this.setResult(HttpStatus.OK, 'Ok', true, this.mapMessageSchemaToDto(data));
                 }
             )
             .catch(
@@ -115,5 +114,22 @@ export class MessagesService {
         this.serviceResult.message = message;
         this.serviceResult.success = success;
         this.serviceResult.data = data;
+    }
+
+    private mapMessageSchemaToDto(messages: any[]) {
+        const output = messages.map(
+            function (message) {
+                return {
+                    "id": message._id,
+                    "fullName": message.fullName,
+                    "company": message.company,
+                    "prefCommunication": message.prefCommunication,
+                    "email": message.email,
+                    "phoneNumber": message.phoneNumber,
+                    "messageText": message.messageText
+                };
+            }
+        );
+        return output;
     }
 }
