15e5c3c1a80781079228ee82d17d747633c0ec8c
Volodia <volodymyr.m@eivolo.com>
2021-06-10 09:50:24 +0300

    added Injectable for ServiceResult

--- src/messages/messages.module.ts
+++ src/messages/messages.module.ts
@@ -3,10 +3,11 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { MessagesController } from './messages.controller';
 import { MessagesService } from './messages.service';
 import { MessageSchema } from './schemas/message.schema';
+import { ServiceResult } from './ServiceResult';
 
 @Module({
     imports: [MongooseModule.forFeature([{ name: 'Message', schema: MessageSchema }])],
     controllers: [MessagesController],
-    providers: [MessagesService],
+    providers: [MessagesService, ServiceResult],
 })
 export class MessagesModule {}
