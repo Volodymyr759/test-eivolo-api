5a9bf692b29e11529c236153b480aed8294d243c
Volodia <volodymyr.m@eivolo.com>
2021-06-10 10:27:02 +0300

    changes

--- src/messages/serviceResult.ts
+++ src/messages/serviceResult.ts
@@ -3,16 +3,8 @@ import { Injectable, Type } from '@nestjs/common';
 @Injectable()
 export class ServiceResult<T extends Type<unknown>> {
     // Members
-    httpStatus?: number;
-    message: string;
-    success: boolean;
+    httpStatus?: number = null;
+    message: string = '';
+    success: boolean = false;
     data?: T[];
-
-    // Ctor
-    constructor() {
-        this.httpStatus = null;
-        this.message = "";
-        this.success = false;
-        this.data = null;
-    }
 }
